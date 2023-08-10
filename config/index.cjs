188627b528e2b4533e0a6567ec0bf3bc86967580
shjnb <76170758+shjnb@users.noreply.github.com>
2023-08-10 15:43:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oOkDB6vHbaBaGyX68jpuENd9QvLA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'H5XrbvchnSS6C7kxiO2R-cMXdDe-QyD7vsznG5xOE-A',
+      useTemplateId: 'QzynfMpSBctkEm_-Kk3e-IhL2Pbqu2ldvkgzd7GRsJY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-05',
       festivals: [
